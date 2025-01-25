efb473d46931e079fef62153807ee39d0a74e56e
hisham rehab <hishamrehab309@gmail.com>
2025-01-25 13:43:12 +0200

    finish the project

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import goalsImg from "./assets/goals.jpg";
+import Header from "./components/Header.tsx";
+import CourseGoalList from "./components/CourseGoalList.tsx";
+import NewGoal from "./components/NewGoal.tsx";
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
+export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal(gaol: string, summary: string) {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: gaol,
+        description: summary,
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) =>
+      prevGoals.filter((goal) => goal.id !== id)
+    );
+  }
+
+  return (
+    <main>
+      <Header image={{ src: goalsImg, alt: "A list of Goals" }}>
+        <h1>Your Course Goals</h1>
+        <NewGoal onAddGoal={handleAddGoal} />
+        <CourseGoalList
+          goals={goals}
+          onDeleteGoal={handleDeleteGoal}
+        />
+      </Header>
+    </main>
+  );
+}
